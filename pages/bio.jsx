4df3471d19460de9991551ed9adb17d7bc7b3f14
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-01-06 18:15:18 +0530

    new

---
+++ pages/bio.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react'
+
+const Bio = ({data}) => {
+    console.log(data)
+  return (
+    <div>
+       <h3>{data.name}</h3>
+       <Image src={data.avatar_url} alt="fg" width={100} height={100}/>
+    </div>
+  )
+}
+export async function getStaticProps(context) {
+  const res = await fetch("https://api.github.com/users/riteshf");
+  const data = await res.json();
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
+export default Bio
